Editted CNN network for future evaluation test purpose / Added input normalization preprocessing / Configured CNN network on training mode without gradient updates as a temporary fix for evaluation mode issue / Fixed training mode for pre-trained model

NN01_Branched_CNN.py
@@ -9,23 +9,6 @@ from matplotlib import pyplot as plt
 
 class Branched_CNN(nn.Module):
 
-    # Stages of Convolutional Layer
-    def conv_layer(self, layer_num, in_channel, out_channel, kernel_size, stride, padding, dropout_rate=0.0, use_batchNorm=True, use_Activation=True):
-
-        conv = nn.Sequential()
-        conv.add_module('conv_'+layer_num, nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride, padding=padding, bias=False))
-
-        if use_Activation:
-            conv.add_module('LeakyReLU_'+layer_num, nn.LeakyReLU(0.1))
-
-        if use_batchNorm:
-            conv.add_module('batchNorm_'+layer_num, nn.BatchNorm2d(out_channel))
-
-        if dropout_rate > 0.0:
-            conv.add_module('dropout_'+layer_num, nn.Dropout(dropout_rate))
-
-        return conv
-
     # Overriding base class of neural network (nn.Module)
     def __init__(self):
         super(Branched_CNN, self).__init__()
@@ -36,23 +19,29 @@ class Branched_CNN(nn.Module):
         self.display_layer_list = ['']      ### Activate layer result display function by user's choice ###
         ###################################################################################################
 
-        # Batch Normalization Preprocessing Layer
-        # self.batch_norm0 = nn.BatchNorm2d(6)
-
-        self.conv1 = self.conv_layer('1', 6, 6, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2), dropout_rate=0.5, use_batchNorm=False, use_Activation=True)
+        self.conv1 = nn.Conv2d(in_channels=6, out_channels=6, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2), bias=False)
+        self.leakyrelu1 = nn.LeakyReLU(0.1)
+        self.batchnorm1 = nn.BatchNorm2d(6)
+        self.dropout1 = nn.Dropout(0.5)
         self.avgpool1 = nn.AvgPool2d(kernel_size=3)
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=1)
 
-        self.conv2 = self.conv_layer('2', 6, 30, kernel_size=(5, 5), stride=(1, 1), padding=(1, 1), dropout_rate=0.5, use_batchNorm=False, use_Activation=True)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=30, kernel_size=(5, 5), stride=(1, 1), padding=(1, 1), bias=False)
+        self.leakyrelu2 = nn.LeakyReLU(0.1)
+        self.dropout2 = nn.Dropout(0.5)
         self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=1)
 
-        self.conv3 = self.conv_layer('3', 30, 30, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), dropout_rate=0.5, use_batchNorm=False, use_Activation=True)
+        self.conv3 = nn.Conv2d(in_channels=30, out_channels=30, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        self.leakyrelu3 = nn.LeakyReLU(0.1)
+        self.dropout3 = nn.Dropout(0.5)
         self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=1)
         
-        self.conv4 = self.conv_layer('4', 30, 150, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), dropout_rate=0.5, use_batchNorm=False, use_Activation=True)
+        self.conv4 = nn.Conv2d(in_channels=30, out_channels=150, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        self.leakyrelu4 = nn.LeakyReLU(0.1)
+        self.dropout4 = nn.Dropout(0.5)
         
         self.fc_1 = nn.Linear(in_features = 205 * 56 * 150, out_features = 12)  # Fully Connected Layer 1
-        self.fc_2 = nn.Linear(in_features = 12, out_features = 6)  # Fully Connected Layer 2
+        self.fc_2 = nn.Linear(in_features = 12, out_features = 6)               # Fully Connected Layer 2
         
         
     # CNN Layer Result Display Function - Display 2D Convolution Results by Channels
@@ -69,26 +58,32 @@ class Branched_CNN(nn.Module):
 
     # Foward pass of DeepVO NN
     def forward(self, x):
-        
-        # Batch Normalization Preprocessing Layer
-        #x = self.batch_norm0(x)
 
         # Forward pass through Common CNN Layer 1
         x = self.conv1(x)
+        x = self.leakyrelu1(x)
+        x = self.batchnorm1(x)
+        x = self.dropout1(x)
         x = self.avgpool1(x)
         x = self.maxpool1(x)
 
         x = self.conv2(x)
+        x = self.leakyrelu2(x)
+        x = self.dropout2(x)
         x = self.maxpool2(x)
 
         # Forward pass through Common CNN Layer 2
         x = self.conv3(x)
+        x = self.leakyrelu3(x)
+        x = self.dropout3(x)
         x = self.maxpool3(x)
 
         x = self.conv4(x)
+        x = self.leakyrelu4(x)
+        x = self.dropout4(x)
         #self.layer_disp(x)
 
-        # print(x.size())   # Print the size of CNN output in order connect it to Fully Connected Layer
+        #print(x.size())   # Print the size of CNN output in order connect it to Fully Connected Layer
         # Reshpae/Flatten the output of common CNN
         x = x.view(x.size(0), -1)
 

model_tester.py
@@ -75,7 +75,21 @@ class tester():
 
         self.NN_model.to(self.PROCESSOR)
 
-        self.NN_model.eval()
+        self.NN_model.train()   # For unknown reason, switching to evaluation mode greatly degrades the performance of the model.
+                                # Also, it seems that evaluation mode does not turn off batch normalizatio in the model. (track_running_stats stays True even after eval())
+                                # Reference : https://discuss.pytorch.org/t/bug-weird-behavior-between-evaluation-and-training-mode/13297/18
+                                #           : https://discuss.pytorch.org/t/bug-weird-behavior-between-evaluation-and-training-mode/13297/18
+                                #           : https://gldmg.tistory.com/124
+                                # In order to temporarily address this issue, the model stays on traning mode, but it does not update any type of gradients using optimizers.
+
+        #self.NN_model.eval()
+        # for layer in self.NN_model.children():
+        #     print(layer)
+        #     if type(layer) == nn.BatchNorm2d:
+        #         print(type(layer))
+        #         print('Disable BatchNorm')
+        #         layer.track_running_stats = True
+
 
         self.test_loader_list = []
         for i in range(len(test_sequence)):
@@ -120,8 +134,9 @@ class tester():
 
         for epoch in range(self.test_epoch):
             
+            # Prevent the model from updating gradients by adding 'torch.no_grad()' & not using optmizer steps
             with torch.no_grad():
-
+                
                 print('[EPOCH] : {}'.format(epoch))
 
                 loss_sum = 0.0

model_trainer.py
@@ -18,7 +18,7 @@ import sys
 
 class trainer():
 
-    def __init__(self, NN_model=None,
+    def __init__(self, NN_model=None, checkpoint=None,
                        use_cuda=True, cuda_num='',
                        loader_preprocess_param=transforms.Compose([]), 
                        model_path='./',
@@ -71,9 +71,6 @@ class trainer():
             self.NN_model.to(self.PROCESSOR)
             self.model_path = './'
 
-        self.NN_model.train()
-        self.NN_model.training = True
-
         self.train_loader_list = []
         for i in range(len(train_sequence)):
             self.train_loader_list.append(torch.utils.data.DataLoader(voDataLoader(img_dataset_path=self.img_dataset_path,
@@ -90,7 +87,24 @@ class trainer():
 
         self.optimizer = optim.Adam(self.NN_model.parameters(), lr=self.learning_rate, weight_decay=0.001, amsgrad=True)
 
-        #summary(self.NN_model, Variable(torch.zeros((1, 6, 384, 1280)).to(self.PROCESSOR)))
+        # summary(self.NN_model, Variable(torch.zeros((1, 6, 384, 1280)).to(self.PROCESSOR)))
+
+        ### Model reloading ###
+        if checkpoint != None:
+            print('Re-training')
+
+            if NN_model == None:
+
+                sys.exit('[Tester ERROR] No NN model is specified')
+
+            else:
+
+                self.NN_model = NN_model
+                self.NN_model.load_state_dict(checkpoint['model_state_dict'])
+                self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+                self.epoch = checkpoint['epoch']
+                self.loss = checkpoint['loss']
+                self.model_path = './'
 
         # Prepare Email Notifier
         self.notifier = notifier_Outlook(sender_email=self.sender_email, sender_email_pw=self.sender_pw)
@@ -116,6 +130,8 @@ class trainer():
             loss_sum = 0.0
 
             before_epoch = time.time()
+
+            self.NN_model.train(True)
 
             for train_loader in self.train_loader_list:
 
@@ -206,6 +222,8 @@ class trainer():
                         # print('loss R : {}'.format(loss_R.item()))
 
             after_epoch = time.time()
+
+            self.NN_model.train(False)
 
             training_loss.append(loss_sum)
             print('Epoch {} Complete | Time Taken : {:.2f} min'.format(epoch, (after_epoch-before_epoch)/60))